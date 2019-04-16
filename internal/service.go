a042a5406ad177223af8d4b66c7b71e8368a990d
Jose Duarte <jmg.duarte@campus.fct.unl.pt>
2019-04-16 23:45:07 +0100

    Add String to service.go

--- internal/service.go
+++ internal/service.go
@@ -1,19 +1,35 @@
 package internal
 
-const (
-	Status     = "status.json"
-	Components = "components.json"
+import (
+	"encoding/json"
+	"log"
 )
 
 type Service struct {
 	// ID string
-	Name     string
-	Endpoint string
+	Name     string `json:"name"`
+	Endpoint string `json:"endpoint"`
+	History  `json:"history,omitempty"`
 }
 
 func NewService(service map[string]string) Service {
 	return Service{
 		Name:     service["name"],
 		Endpoint: service["endpoint"],
+		History:  make(History),
 	}
+}
+
+func (s Service) String() string {
+	var res struct {
+		Name     string `json:"name"`
+		Endpoint string `json:"endpoint"`
+	}
+	res.Name = s.Name
+	res.Endpoint = s.Endpoint
+	b, err := json.MarshalIndent(&res, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(b)
 }
